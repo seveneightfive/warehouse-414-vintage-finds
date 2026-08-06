0ce583e308939b614b12734a50086bde8fac0e85
seveneightfive <35607452+seveneightfive@users.noreply.github.com>
2026-08-06 14:57:22 -0500

    Change link text from 'Curated Sets' to 'Homepage Banners'

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -304,7 +304,7 @@ const AdminSidebar = () => {
               `}
             >
               <BookOpen size={18} />
-              Curated Sets
+              Homepage Banners
             </Link>
 
             <Link
